72d39ce04306bdeb5fdfa7b124e7258e49d5c377
Austin Marsh <redausty@live.com>
2023-06-27 22:25:47 -0400

    added additional bootstrap styling

--- db/migrate/20230628011112_create_to_do_lists.rb
+++ db/migrate/20230628011112_create_to_do_lists.rb
@@ -2,7 +2,7 @@ class CreateToDoLists < ActiveRecord::Migration[7.0]
   def change
     create_table :to_do_lists do |t|
       t.string :description
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
